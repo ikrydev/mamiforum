870e91d84d5598733850d5d8b7f6ba050393c12e
ikrydev <ikrydev@gmail.com>
2020-05-22 23:58:25 +0700

    fix: fix layout thread-show's page

--- src/views/PageThreadShow.vue
+++ src/views/PageThreadShow.vue
@@ -1,14 +1,12 @@
 <template>
-  <div>
-    <div class="col-large push-top">
-      <h1>{{thread.title}}</h1>
-      <p>
-        By <a href="#" class="link-unstyled">{{user.name}}</a>, <app-date :timestamp="thread.publishedAt"></app-date>.
-        <span style="float:right; margin-top: 2px;" class="hide-mobile text-faded text-small">{{repliesCount}} replies by 3 contributors</span>
-      </p>
-      <post-list :posts="posts"></post-list>
-      <post-editor :threadId="id"></post-editor>
-    </div>
+  <div class="col-large push-top">
+    <h1>{{thread.title}}</h1>
+    <p>
+      By <a href="#" class="link-unstyled">{{user.name}}</a>, <app-date :timestamp="thread.publishedAt"></app-date>.
+      <span style="float:right; margin-top: 2px;" class="hide-mobile text-faded text-small">{{repliesCount}} replies by 3 contributors</span>
+    </p>
+    <post-list :posts="posts"></post-list>
+    <post-editor :threadId="id"></post-editor>
   </div>
 </template>
 
